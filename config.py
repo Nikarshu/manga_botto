0e1d7bd94913022901183faa8f0437f05b88488e
Nikarshu <83594636+Nikarshu@users.noreply.github.com>
2022-07-03 13:41:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #* ----------------> Telegram Variables <-----------------
-CACHE_CHANNEL:int = -1001575050185 #? Admin Group ID 
-CHANNEL:int = -1001572181436 #? Main Channel ID
+CACHE_CHANNEL:int = -1001524969390 #? Channel that will be used to only ask for a chapter once
+CHANNEL:int = -1001524969390 #? [Optional] Create a new channel (public), copy username wihtout t.me/
 
 #* -----------------> Credentials - https://my.telegram.org <------------------------
 API_ID:int = 4329428 #? Telegram API ID
 API_HASH:str = 'da3cda55bca8a7d7cc14e5d294a62d0e' #? Telegram API Hash 
-BOT_TOKEN:str = '1972464791:AAHZ-9Aaz6pvx8tF0lG_0xDEux7e2T7uzko' #? Telegram Bot Token - t.me/botfather 
+BOT_TOKEN:str = '5477311912:AAF_OgLIvE9XxqHt9bzcYxCQbhRHG_43Wck' #? Telegram Bot Token - t.me/botfather 
