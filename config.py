b67824fdca3d6d3d97695e73248dbaaa1a06d718
Nikarshu <83594636+Nikarshu@users.noreply.github.com>
2022-07-03 13:38:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,8 @@
 #* ----------------> Telegram Variables <-----------------
-ADMINCHAT:int = -1001575050185 #? Admin Group ID 
+CACHE_CHANNEL:int = -1001575050185 #? Admin Group ID 
 CHANNEL:int = -1001572181436 #? Main Channel ID
-GROUP:int = -1001363546811 #? Main Group ID
-BOT_USERNAME:str = 'WCC_postBOT' #? Example: 'anidex' no "@"
-CHANNEL_USERNAME:str = 'weebs_chitchat' #? Example: 'anabysss' no "@"
-LINK:str= f'https://t.me/'+CHANNEL_USERNAME+'/{}?thread={}' #? No Edit 
-
-#* -----------------> Users <------------------------
-OWNER:int = 1802267067 #? Example :- 1183697491
-SUDO:list = [OWNER, 871924423] #? Sudo Users 
 
 #* -----------------> Credentials - https://my.telegram.org <------------------------
 API_ID:int = 4329428 #? Telegram API ID
 API_HASH:str = 'da3cda55bca8a7d7cc14e5d294a62d0e' #? Telegram API Hash 
 BOT_TOKEN:str = '1972464791:AAHZ-9Aaz6pvx8tF0lG_0xDEux7e2T7uzko' #? Telegram Bot Token - t.me/botfather 
-
-#* -------------------> MongoDB - https://www.mongodb.com/try <------------------------
-MONGO_URI:str = 'mongodb+srv://Wcpostbot:Wcpostbot@cluster0.4k0ww.mongodb.net/cluster0?retryWrites=true&w=majority' #? MongoDB Connection String
-DB:str = 'WCC_postBOT' #? Example :- 'PostDB'
