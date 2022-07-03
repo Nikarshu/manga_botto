93d3bbac04b68db3ec236ab78a81830234c37eff
Nikarshu <83594636+Nikarshu@users.noreply.github.com>
2022-07-03 14:05:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#* ----------------> Telegram Variables <-----------------
-CACHE_CHANNEL:int = -1001524969390 #? Channel that will be used to only ask for a chapter once
-CHANNEL:int = -1001524969390 #? [Optional] Create a new channel (public), copy username wihtout t.me/
-
-#* -----------------> Credentials - https://my.telegram.org <------------------------
-API_ID:int = 4329428 #? Telegram API ID
-API_HASH:str = 'da3cda55bca8a7d7cc14e5d294a62d0e' #? Telegram API Hash 
-BOT_TOKEN:str = '5477311912:AAF_OgLIvE9XxqHt9bzcYxCQbhRHG_43Wck' #? Telegram Bot Token - t.me/botfather 
